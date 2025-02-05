2308591dbe77344051a14b381b7dcef8d19ad560
RedHeadSec <42355245+RedHeadSec@users.noreply.github.com>
2025-02-05 14:09:34 -0600

    Cerbero Library Implementation

--- src/kerberos/cerberos.rs
+++ src/kerberos/cerberos.rs
@@ -10,7 +10,7 @@ pub struct CerberoOutput {
 }
 
 pub fn run_cerbero(cerbero_args: &[&str]) -> Result<CerberoOutput, Box<dyn std::error::Error>> {
-    init_log(2); // Default to level 2
+    init_log(1); // Default to level 2
     let mut full_args = vec!["cerbero"]; // This acts as a [0] placeholder for Cerbero's clap implementation for arguments. 
     full_args.extend_from_slice(cerbero_args); // appends user agruments to be parsed by Clap. 
 
