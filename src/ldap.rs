ad4674b2305dbf30e1ccf9c3dfd2f1439ac3cb41
RedHeadSec <42355245+RedHeadSec@users.noreply.github.com>
2025-02-05 16:50:55 -0600

    Kerberos Env Check

--- src/ldap.rs
+++ src/ldap.rs
@@ -40,7 +40,7 @@ pub fn ldap_connect(config: &LdapConfig) -> Result<(LdapConn, String)> {
             Ok(ccache) => println!("[DEBUG] KRB5CCNAME is set: {}", ccache),
             Err(_) => {
                 eprintln!("[ERROR] KRB5CCNAME is NOT set! Kerberos may fail.");
-                return Err("KRB5CCNAME is not set.".into());  // Use `into()` to convert into a boxed error
+                return Err(Box::<dyn std::error::Error>::from("KRB5CCNAME is not set."));
             }
         }
         ldap.sasl_gssapi_bind(&config.dc_ip)?.success()?; // Use GSSAPI (Kerberos) for authentication
